3ce874e968fbed71f9c6c678a2b1cc0b30e98ddc
JieDimension <3348149202@qq.com>
2026-07-22 07:08:01 +0800

    Add Dimension Defender privacy policy page

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -28,6 +28,9 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 	// 关于
 	links.push(LinkPresets.About);
 
+	// 隐私政策
+	links.push(LinkPresets.Privacy);
+
 	// 外部链接
 	links.push({
 		name: "链接",
@@ -85,6 +88,11 @@ export const LinkPresets: Record<string, NavBarLink> = {
 		name: "关于",
 		url: "/about/",
 		icon: "material-symbols:person",
+	},
+	Privacy: {
+		name: "隐私政策",
+		url: "/privacy/",
+		icon: "material-symbols:shield-lock-outline",
 	},
 };
 
